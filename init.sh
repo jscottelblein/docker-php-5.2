3ce89dba0f231ef29497561e88d7b402231fdd4e
J. Scott Elblein <stardogg@gmail.com>
2026-04-07 22:43:29 -0500

    Fix init.sh by removing redundant service start command

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm -rf /opt/httpd-2.2.3/logs/httpd.pid
+
 echo "" > /var/log/php/error.log
 echo "" > /var/log/php/xdebug.log
 echo "" > /var/log/apache/access_log
@@ -8,5 +9,4 @@ echo "" > /var/log/apache/error_log
 
 tail -f /var/log/apache/access_log > /dev/stdout & \
 tail -f /var/log/php/error.log /var/log/apache/error_log > /dev/stderr & \
-service ssh start & \
 /opt/httpd-2.2.3/bin/apachectl -D FOREGROUND
